d71ff32108bd46dada18d0f5f9bd29bbbaeab81b
Ian Handel <ianhandel@users.noreply.github.com>
2017-09-07 17:31:06 +0100

    Update NASAMarine_i2c_to_daughter_display_serial_in.ino

--- NASAMarine_i2c_to_daughter_display_serial_in.ino
+++ NASAMarine_i2c_to_daughter_display_serial_in.ino
@@ -1,3 +1,12 @@
+/*
+Code to read output from chartplotter (NMEA DPT sentence)
+Then parse out depth
+Convert to 7 segments etc 
+Then write out to NASA Depth Repeater
+I2C bit banging and 7 segment code from modified from Peter Holtermann
+See... http://wiki.openseamap.org/wiki/De:NASA_Clipper_Range
+*/
+
 #include <TinyGPS++.h>
 TinyGPSPlus gps;
 
